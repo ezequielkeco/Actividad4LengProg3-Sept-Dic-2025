da5ce153522fdabc56fc2a416aeb1a8622bc321c
Ezequiel Santos <ezequielsantosg@gmail.com>
2025-10-24 14:34:36 -0400

    Creación modelo Recinto
    Creación del modelo Recinto con sus modelo de datos Codigo y Nombre.

---
+++ Actividad4LengProg3/Models/RecintoViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Actividad4LengProg3.Models
+{
+    public class RecintoViewModel
+    {
+        [Required(ErrorMessage = "Debe proporcionar el código del recinto.")]
+        public string Codigo { get; set; }
+
+        [Required(ErrorMessage = "Debe proporcionar el nombre del recinto.")]
+        [StringLength(100, ErrorMessage = "El nombre no puede exceder los 100 caracteres.")]
+        public string Nombre { get; set; }
+    }
+}

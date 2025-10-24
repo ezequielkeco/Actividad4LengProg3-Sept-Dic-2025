d85bd2064c27e2c5ab16ee77314374adb77dc925
Ezequiel Santos <ezequielsantosg@gmail.com>
2025-10-24 19:03:30 -0400

    Actualización
    Se actualizaron parámetros en el modelo Estudiante y las vistas: Index y Editar.

--- Actividad4LengProg3/Models/EstudianteViewModel.cs
+++ Actividad4LengProg3/Models/EstudianteViewModel.cs
@@ -1,53 +1,52 @@
-﻿using System;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace Actividad4LengProg3.Models
 {
     public class EstudianteViewModel
     {
-        [Required(ErrorMessage = "Debe proporcionar su nombre completo")]
-        [StringLength(100)]
+        [Required(ErrorMessage = "Proporcione el nombre completo")]
+        [StringLength(100, ErrorMessage = "El nombre completo no puede exceder los 100 caracteres")]
         public string NombreCompleto { get; set; }
 
         [Required]
-        [StringLength(200)]
+        [StringLength(200, ErrorMessage = "La dirección no puede exceder los 200 caracteres")]
         public string Direccion { get; set; }
 
         [Phone]
-        [MinLength(10)]
+        [MinLength(10, ErrorMessage = "El número de celular no puede exceder los 10 caracteres")]
         public string Celular { get; set; }
 
         [Phone]
-        [MinLength(10)]
+        [MinLength(10, ErrorMessage = "El número de teléfono no puede exceder los 10 caracteres")]
         public string Telefono { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Indique la fecha de nacimiento")]
         [DataType(DataType.Date)]
         public DateTime FechaNacimiento { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Debe indicar el género")]
         public string Genero { get; set; }
 
-        [Required]
-        [StringLength(15, MinimumLength = 6)]
+        [Required(ErrorMessage = "Debe indicar una matrícula")]
+        [StringLength(15, MinimumLength = 6, ErrorMessage = "La matrícula está comprendida entre 6 y  15 caracteres")]
         public string Matricula { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Debe seleccionar la carrera")]
         public string Carrera { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Debe seleccionar el campus")]
         public string Campus { get; set; }
 
-        [Required]
-        [EmailAddress]
+        [Required(ErrorMessage = "Indique el correo institucional")]
+        [EmailAddress(ErrorMessage = "El correo no tiene un formato válido")]
         public string Correo { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Seleccione una tanda")]
         public string Tanda { get; set; }
 
         public bool EstaBecado { get; set; }
 
-        [Range(0, 100)]
+        [Range(0, 100, ErrorMessage = "El porcentaje debe estar entre 0 y 100")]
         public int? PorcentajeBeca { get; set; }
     }
 }

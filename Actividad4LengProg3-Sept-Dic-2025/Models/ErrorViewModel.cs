d39e2b30f06fcb752f37a3a578429a9f3bcf0791
Ezequiel Santos <ezequielsantosg@gmail.com>
2025-10-23 23:40:15 -0400

    Inicio del proyecto

---
+++ Actividad4LengProg3-Sept-Dic-2025/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Actividad4LengProg3_Sept_Dic_2025.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

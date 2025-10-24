1991b112166094d89cde13f1ad8ae625c7e94ed9
Ezequiel Santos <ezequielsantosg@gmail.com>
2025-10-23 23:44:45 -0400

    Modelo Estudiante
    Creación del modelo Estudiante

---
+++ Actividad4LengProg3-Sept-Dic-2025/Models/EstudianteViewModel.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Actividad3LengProg3.Models
+{
+    public class EstudianteViewModel
+    {
+        [Required(ErrorMessage = "Debe proporcionar su nombre completo")]
+        [StringLength(100)]
+        public string NombreCompleto { get; set; }
+
+        [Required]
+        [StringLength(200)]
+        public string Direccion { get; set; }
+
+        [Phone]
+        [MinLength(10)]
+        public string Celular { get; set; }
+
+        [Phone]
+        [MinLength(10)]
+        public string Telefono { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime FechaNacimiento { get; set; }
+
+        [Required]
+        public string Genero { get; set; }
+
+        [Required]
+        [StringLength(15, MinimumLength = 6)]
+        public string Matricula { get; set; }
+
+        [Required]
+        public string Carrera { get; set; }
+
+        [Required]
+        public string Campus { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Correo { get; set; }
+
+        [Required]
+        public string Tanda { get; set; }
+
+        public bool EstaBecado { get; set; }
+
+        [Range(0, 100)]
+        public int? PorcentajeBeca { get; set; }
+    }
+}

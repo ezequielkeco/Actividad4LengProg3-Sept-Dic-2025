5265fc4a78a618cad47d28c0641b02c256387ac7
Ezequiel Santos <ezequielsantosg@gmail.com>
2025-10-24 14:37:19 -0400

    Creación modelo Carrera
    Creación del modelo Carrera con sus campos de datos Codigo, Nombre, CantidadCreditos y CantidadMaterias.

---
+++ Actividad4LengProg3/Models/CarreraViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Actividad4LengProg3.Models
+{
+    public class CarreraViewModel
+    {
+        [Required(ErrorMessage = "Debe proporcionar el código de la carrera.")]
+        public string Codigo { get; set; }
+
+        [Required(ErrorMessage = "Debe proporcionar el nombre de la carrera.")]
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "Debe indicar la cantidad de créditos.")]
+        public int CantidadCreditos { get; set; }
+
+        [Required(ErrorMessage = "Debe indicar la cantidad de materias.")]
+        public int CantidadMaterias { get; set; }
+    }
+}

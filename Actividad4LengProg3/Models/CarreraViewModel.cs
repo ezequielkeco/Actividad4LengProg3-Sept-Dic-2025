8fd593f33238f7fbbee197e1eed842ef4d91f600
Ezequiel Santos <ezequielsantosg@gmail.com>
2025-10-24 22:26:51 -0400

    Actualización
    Se actualizaron los campos y tipos de datos.

--- Actividad4LengProg3/Models/CarreraViewModel.cs
+++ Actividad4LengProg3/Models/CarreraViewModel.cs
@@ -4,13 +4,13 @@ namespace Actividad4LengProg3.Models
 {
     public class CarreraViewModel
     {
-        [Required(ErrorMessage = "Debe proporcionar el código de la carrera.")]
+        [Required(ErrorMessage = "Proporcione el código de la carrera.")]
         public string Codigo { get; set; }
 
-        [Required(ErrorMessage = "Debe proporcionar el nombre de la carrera.")]
+        [Required(ErrorMessage = "Proporcione el nombre de la carrera.")]
         public string Nombre { get; set; }
 
-        [Required(ErrorMessage = "Debe indicar la cantidad de créditos.")]
+        [Required(ErrorMessage = "Indique la cantidad de créditos.")]
         public int CantidadCreditos { get; set; }
     }
 }
